42aaae133da234f886fd2c9dfa64c4bad704cf96
kiptook7070 <kiptook2020@gmail.com>
2023-07-22 10:40:52 +0300

    changes

--- kafka-consumer/src/main/java/com/eclectics/io/kafkaservice/KafkaConsumerConfig.java
+++ kafka-consumer/src/main/java/com/eclectics/io/kafkaservice/KafkaConsumerConfig.java
@@ -1,17 +1,12 @@
 package com.eclectics.io.kafkaservice;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 import java.util.HashMap;
 import java.util.Map;
